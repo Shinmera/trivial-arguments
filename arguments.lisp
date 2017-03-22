430366783cd918394f0ace3127f31e82876d90fc
Shinmera <shinmera@tymoon.eu>
2017-03-22 23:58:53 +0100

    Cleaned up a bit, added support for cmucl and corman. Removed optional swank dependency. Fixes #2

--- arguments.lisp
+++ arguments.lisp
@@ -11,55 +11,77 @@
    #:arglist))
 (in-package #:org.shirakumo.trivial-arguments)
 
-#-(or :swank :abcl :allegro :ccl :clisp :ecl :lispworks :sbcl :scl :clasp)
-(warn "TRIVIAL-ARGUMENTS NOTICE: SWANK not present and implementation not directly supported. Falling back to FUNCTION-LAMBDA-EXPRESSION.")
+#-(or :abcl :allegro :ccl :clasp :clisp :cmucl :corman :ecl :lispworks :sbcl :scl)
+(warn "TRIVIAL-ARGUMENTS NOTICE: Your implementation is not directly supported. Falling back to FUNCTION-LAMBDA-EXPRESSION.")
+
+(defmacro with-unknown-on-error (&body body)
+  `(handler-case (progn ,@body)
+     (error () :unknown)))
 
 (defun arglist (function)
   "Returns the lambda-list of the function if possible, :unknown otherwise."
   (let ((function (etypecase function
                     ((or list symbol function) function))))
-    #+:swank
-    (let ((result (#.(find-symbol "ARGLIST" (cond ((find-package "SWANK-BACKEND") "SWANK-BACKEND")
-                                                  ((find-package "SWANK/BACKEND") "SWANK/BACKEND")
-                                                  (T (error "Swank backend package could not be found."))))
-                     function)))
-      (if (eq result :not-provided) :unknown result))
-
-    #+(and :abcl (not :swank))
+    #+:abcl
     (multiple-value-bind (list provided) (sys::arglist function)
       (if provided list :unknown))
 
-    #+(and :allegro (not :swank))
-    (or (ignore-errors (excl:arglist function))
-        :unknown)
+    #+:allegro
+    (with-unknown-on-error
+      (excl:arglist function))
 
-    #+(and :ccl (not :swank))
+    #+:ccl
     (multiple-value-bind (list provided) (ccl:arglist function)
       (if provided list :unknown))
 
-    #+(and :clisp (not :swank))
-    (or (ignore-errors (ext:arglist function))
-        :unknown)
+    #+:clasp
+    (multiple-value-bind (list provided) (core:function-lambda-list function)
+      (if provided list :unknown))
 
-    #+(and :ecl (not :swank) (not :clasp))
+    #+:clisp
+    (with-unknown-on-error
+      (ext:arglist function))
+
+    #+:cmucl
+    (with-unknown-on-error
+      (cond ((eval:interpreted-function-p function)
+             (eval:interpreted-function-arglist function))
+            ((pcl::generic-function-p function)
+             (pcl:generic-function-lambda-list function))
+            ((c::byte-function-or-closure-p function)
+             (byte-code-function-arglist function))
+            ((kernel:%function-arglist (kernel:%function-self function))
+             (read-arglist function))
+            (T
+             (debug-function-arglist (di::function-debug-function function)))))
+
+    #+:corman
+    (with-unknown-on-error
+      (cond ((and (symbolp function) (macro-function function))
+             (ccl::macro-lambda-list (symbol-function function)))
+            (T
+             (when (symbolp function)
+               (setf function (symbol-function function)))
+             (if (eq (class-of name) cl::the-class-standard-gf)
+                 (generic-function-lambda-list name)
+                 (ccl:function-lambda-list name)))))
+
+    #+:ecl
     (multiple-value-bind (list provided) (ext:function-lambda-list function)
       (if provided list :unknown))
 
-    #+(and :lispworks (not :swank))
+    #+:lispworks
     (let ((list (lw:function-lambda-list function)))
       (if (eq list :dont-know) :unknown list))
     
-    #+(and :sbcl (not :swank))
+    #+:sbcl
     (sb-introspect:function-lambda-list function)
 
-    #+(and :scl (not :swank))
+    #+:scl
     (multiple-value-bind (list provided) (ext:function-arglist function)
       (if provided list :unknown))
 
-    #+(and :clasp (not :swank))
-    (core:function-lambda-list function)
-
-    #-(or :swank :abcl :allegro :ccl :clisp :ecl :lispworks :sbcl :scl :clasp)
+    #-(or :abcl :allegro :ccl :clasp :clisp :cmucl :corman :ecl :lispworks :sbcl :scl)
     (let ((lambda (function-lambda-expression (etypecase function
                                                 ((or list symbol) (fdefinition function))
                                                 (function function)))))
